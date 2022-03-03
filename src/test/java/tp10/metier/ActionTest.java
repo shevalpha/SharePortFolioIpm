43bf24ab78c46a41a31b70a26eb94f02cc8622fb
21403950 <21403950@10.12.126.206>
2022-03-03 16:36:39 +0100

    commit projet

---
+++ src/test/java/tp10/metier/ActionTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp10.metier;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author 21403950
+ */
+public class ActionTest {
+    
+    public ActionTest() {
+    }
+
+   
+   
+
+    @org.junit.jupiter.api.Test
+    public void testSomeMethod() {
+    }
+    
+}
